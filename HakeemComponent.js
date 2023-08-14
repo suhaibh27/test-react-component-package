eb2285a0015897760d412ec102127ff5237d71b5
suhaib hakeem <s.hakeem@gauge-erp.com>
2023-08-14 11:00:40 +0300

    first commit

---
+++ HakeemComponent.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const HakeemComponent = () => {
+  return <div>Hakeem</div>;
+};
+
+export default HakeemComponent;
---
+++ index.js
@@ -0,0 +1,1 @@
+export { default as HakeemComponent } from './HakeemComponent';
